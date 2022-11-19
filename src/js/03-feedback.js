4bd6b8a50af009b169a5b04743782db348dea374
Bogdan <bogdanberezka@gmail.com>
2022-11-19 16:32:53 +0200

    task3-half-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const formData = {};
+const refs = {
+  formEl: document.querySelector('.feedback-form'),
+  inputEl: document.querySelector('input'),
+  textareaEl: document.querySelector('textarea'),
+  buttonEl: document.querySelector('button'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+startPoint();
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+refs.formEl.addEventListener(
+  'input',
+  throttle(e => {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }, 500)
+);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log('Отправляем форму');
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function startPoint() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedSavedData = JSON.parse(savedData);
+    refs.inputEl.value = parsedSavedData.email;
+    refs.textareaEl.textContent = parsedSavedData.message;
+  }
+}
